3b3b442edde1314eb1f0824c379c95da846faeaf
jolshin <n.jolshin@gmail.com>
2024-04-15 01:28:20 +0800

    First commit

---
+++ first_project/app/views.py
@@ -0,0 +1,42 @@
+import datetime
+
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+
+from os import walk
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    # впишите правильные адреса страниц, используя
+    # функцию `reverse`
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время':  reverse('time'),
+        'Показать содержимое рабочей директории':  reverse('workdir')
+    }
+    
+    # context и параметры render менять не нужно
+    # подбробнее о них мы поговорим на следующих лекциях
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    # обратите внимание – здесь HTML шаблона нет, 
+    # возвращается просто текст
+    current_time = datetime.datetime.now()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    template_name = 'app/workdir.html'
+    filenames = next(walk('.'), (None, None, []))[2]
+
+    context = {
+        'filenames': filenames
+    }
+    return render(request, template_name, context)
